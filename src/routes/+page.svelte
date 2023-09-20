1bf0ebffdb9ee2097db9dc39c74a8617e9d51cf9
KKY <pcaso5@naver.com>
2023-09-20 16:36:42 +0900

    [KKY] gif 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,8 @@
   import delevery from '$lib/images/배달.gif';
   import threeDay from '$lib/images/숙성.gif';
   import haccp from '$lib/images/HACCP_logo.png';
-  import onion from '$lib/images/onion.png';
+  import galic from '$lib/images/마늘.gif';
+  import twice from '$lib/images/2인.gif';
   import Connect from '$lib/components/Connect.svelte';
   import FigureImage from '$lib/components/FigureImage.svelte';
   import IconCard from '$lib/components/IconCard.svelte';
@@ -36,18 +37,36 @@
 
 <section class="flex flex-col items-center">
   <Hero />
-  <article class="py-40 w-full">
-    <h3 class="md:text-3xl sm:text-2xl text-xl font-extrabold uppercase text-center">프랜차이즈 찜앤장</h3>
-    <p class="whitespace-pre-line mx-auto max-w-md">
-      {`- 2인 운영 매장에 최적화된 운영시스템`}
-    </p>
+  <article class="py-40 w-full flex flex-col gap-10">
+    <div class="whitespace-pre-line mx-auto max-w-md text-center font-medium text-2xl md:text-5xl flex flex-col gap-5">
+      <p><span class="text-primary font-extrabold">2인 운영</span>에</p>
+      <p>최적화된 매장 시스템</p>
+    </div>
+    <figure class="aspect-[3/2] overflow-hidden max-w-xl mx-auto">
+      <img src={twice} alt="" />
+    </figure>
   </article>
-  <article>
-    <img src={threeDay} alt="" />
-  </article>
-  <article>
-    <img src={hall} alt="" />
-    <img src={delevery} alt="" />
+  <article class="flex flex-col gap-10 pb-40 relative items-center">
+    <div class="text-center font-medium md:text-5xl text-3xl flex-col gap-5 hidden md:flex w-full md:w-max mx-auto">
+      <p class="text-4xl">
+        <span class="font-extrabold">배달+홀 겸업</span> 매장 / <span class="font-extrabold">배달</span> 매장
+      </p>
+      <p>두가지 중 <span class="text-primary font-extrabold">선택 가능</span></p>
+    </div>
+    <div class="flex max-w-5xl mx-auto justify-between md:gap-10 gap-20 md:flex-row flex-col">
+      <figure>
+        <img src={hall} alt="홀" class="" />
+      </figure>
+      <div
+        class="text-center font-extrabold md:text-5xl text-3xl flex-col gap-2 flex md:hidden w-full md:w-max mx-auto"
+      >
+        <p><span class="text-primary">배달+홀</span> 겸업 매장</p>
+        <p><span class="text-primary">배달</span>매장 두가지 중 <span class="text-primary">선택 가능</span></p>
+      </div>
+      <figure>
+        <img src={delevery} alt="딜리버리" class="" />
+      </figure>
+    </div>
   </article>
   <article class="flex flex-col gap-5 py-40 w-full relative bg-primary">
     <h3 class="text-center md:text-3xl sm:text-2xl text-xl font-extrabold uppercase text-white relative z-10">
@@ -56,7 +75,7 @@
     <div class="flex items-center gap-5 mx-auto relative z-10 md:flex-row flex-col">
       <img data-aos="flip-left" src={fiveMinute} alt="5분" class="w-80" />
       <div>
-        <p class="font-extrabold text-4xl text-white animate-bounce">조리까지 단 <span class="text-white">5분</span></p>
+        <p class="font-extrabold text-5xl text-white animate-bounce">조리까지 단 <span class="text-white">5분</span></p>
         <p class="text-white">누구나 조리 가능한 시스템으로 매장 자동화 가능</p>
       </div>
     </div>
@@ -75,18 +94,24 @@
       <Card src={menu3} title="아구찜" discription={`푸-짐한 양에 매-콤한 맛,\n역대급 아구찜!`} />
     </div>
   </article>
-  <article data-aos="fade-down" class="py-28 w-full flex flex-col gap-10 max-w-[50rem]">
+  <article data-aos="fade-down" class="w-full flex flex-col gap-10 max-w-[50rem]">
     <h3 class="md:text-3xl sm:text-2xl text-xl font-extrabold uppercase text-primary text-center">healthy food</h3>
-    <div class="flex md:flex-row flex-col items-center gap-10">
-      <div class="h-[20rem] w-full mx-auto flex items-center justify-center">
-        <div class="animate-bounce h-[5rem] w-full flex items-center justify-center">
-          <FigureImage src={onion} alt={'국내산 양파'} className="w-full max-w-[27rem] max-h-[20rem] aspect-[27/20]" />
-        </div>
-      </div>
-      <div class="text-center w-full">
+    <div class="flex flex-col items-center relative">
+      <figure class="aspect-square overflow-hidden">
+        <img src={galic} alt={'국내산 마늘'} />
+      </figure>
+      <div class="text-center w-full absolute md:bottom-40 bottom-0">
         <p class="text-3xl font-extrabold animate-pulse text-primary">국내산 마늘로 면역력 업!</p>
-        <p class="text-2xl whitespace-pre-line">{`3일 숙성양념소스로 맛은 물론,\n몸에 좋은 면역력강화 효과까지`}</p>
       </div>
+    </div>
+  </article>
+  <article class="flex flex-col gap-10 pb-40">
+    <figure class="aspect-[3/2] overflow-hidden max-w-xl">
+      <img src={threeDay} alt="" />
+    </figure>
+    <div class="text-2xl md:text-5xl text-center font-extrabold flex flex-col gap-2">
+      <p>3일 <span class="text-primary">숙성 양념 소스</span>로 <span class="text-primary">맛</span>은 물론</p>
+      <p>몸에 좋은 <span class="text-primary">면역력 강화</span> 효과까지</p>
     </div>
   </article>
   <article class="flex flex-col gap-10 pt-40 w-full">
@@ -118,7 +143,7 @@
       </p>
     </div>
   </article>
-  <article class="pt-40 w-full flex flex-col gap-5 px-5">
+  <article class="py-40 w-full flex flex-col gap-5 px-5">
     <h3 class="text-center md:text-3xl sm:text-2xl text-xl font-extrabold uppercase text-primary">가맹계약</h3>
     <div class="bg-base-300 h-20 w-[1px] mx-auto" />
     <p class="text-center whitespace-pre text-lg">
